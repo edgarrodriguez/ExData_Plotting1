8a29bc1b70d051dd58daf5e0c83a97bd243a0d77
Edgar Rodriguez <edgarr@outlook.com>
2014-07-13 12:17:04 -0500

    Final version Project1
    Fixed graphs 1,2,3,4

--- plot3.R
+++ plot3.R
@@ -1,27 +1,32 @@
 ##Code to draw Exploratory Data Analisys Project 1 Plot 3
 
-#dtHouseholdPowerConsumption<-read.csv2("household_power_consumption.txt")
+dtHouseholdPowerConsumption<-read.csv2("household_power_consumption.txt")
 
-#KeyDateTime<-strptime(paste(dtHouseholdPowerConsumption[,"Date"],dtHouseholdPowerConsumption[,"Time"]),"%d/%m/%Y %H:%M:%S")
+KeyDateTime<-strptime(paste(dtHouseholdPowerConsumption[,"Date"],dtHouseholdPowerConsumption[,"Time"]),"%d/%m/%Y %H:%M:%S")
 
-#dtSubset<-cbind(KeyDateTime,dtHouseholdPowerConsumption)
+dtSubset<-cbind(KeyDateTime,dtHouseholdPowerConsumption)
 
-#rm(dtHouseholdPowerConsumption)
+rm(dtHouseholdPowerConsumption)
 
-#IniDateTime<-strptime("2007-02-01 00:00:00" , "%Y-%m-%d %H:%M:%S")
-#FinDateTime<-strptime("2007-02-03 00:00:00" , "%Y-%m-%d %H:%M:%S")
+IniDateTime<-strptime("2007-02-01 00:00:00" , "%Y-%m-%d %H:%M:%S")
+FinDateTime<-strptime("2007-02-03 00:00:00" , "%Y-%m-%d %H:%M:%S")
 
-#dtSelectedDates<-subset(dtSubset,KeyDateTime>=IniDateTime & KeyDateTime<FinDateTime)
+dtSelectedDates<-subset(dtSubset,KeyDateTime>=IniDateTime & KeyDateTime<FinDateTime)
 
-#rm(dtSubset)
-#rm(KeyDateTime)
+rm(dtSubset)
+rm(KeyDateTime)
 
 png(filename = "plot3.png", width = 480, height = 480, units = "px" , bg="transparent")
 
-#Put together the data and use points to print each color
+plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1),ylab="Energy sub metering",xlab="",pch="",ylim=c(0,40))
 
-#plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1),ylab="Energy sub metering",xlab="",type="n")
-plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1),ylab="Energy sub metering",xlab="")
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1))
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_2),col="red",ylim=c(0,40))
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_3),col="blue",ylim=c(0,40))
+
+## Add Legend
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       pch=c("_","_","_"),col=c("black","red","blue"))
 
 
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,33 +1,48 @@
 ##Code to draw Exploratory Data Analisys Project 1 Plot 4
 
-#dtHouseholdPowerConsumption<-read.csv2("household_power_consumption.txt")
+dtHouseholdPowerConsumption<-read.csv2("household_power_consumption.txt")
 
-#KeyDateTime<-strptime(paste(dtHouseholdPowerConsumption[,"Date"],dtHouseholdPowerConsumption[,"Time"]),"%d/%m/%Y %H:%M:%S")
+KeyDateTime<-strptime(paste(dtHouseholdPowerConsumption[,"Date"],dtHouseholdPowerConsumption[,"Time"]),"%d/%m/%Y %H:%M:%S")
 
-#dtSubset<-cbind(KeyDateTime,dtHouseholdPowerConsumption)
+dtSubset<-cbind(KeyDateTime,dtHouseholdPowerConsumption)
 
-#rm(dtHouseholdPowerConsumption)
+rm(dtHouseholdPowerConsumption)
 
-#IniDateTime<-strptime("2007-02-01 00:00:00" , "%Y-%m-%d %H:%M:%S")
-#FinDateTime<-strptime("2007-02-03 00:00:00" , "%Y-%m-%d %H:%M:%S")
+IniDateTime<-strptime("2007-02-01 00:00:00" , "%Y-%m-%d %H:%M:%S")
+FinDateTime<-strptime("2007-02-03 00:00:00" , "%Y-%m-%d %H:%M:%S")
 
-#dtSelectedDates<-subset(dtSubset,KeyDateTime>=IniDateTime & KeyDateTime<FinDateTime)
+dtSelectedDates<-subset(dtSubset,KeyDateTime>=IniDateTime & KeyDateTime<FinDateTime)
 
-#rm(dtSubset)
-#rm(KeyDateTime)4
-
-#Organize then draw 
-
+rm(dtSubset)
+rm(KeyDateTime)
 
 
 png(filename = "plot4.png", width = 480, height = 480, units = "px" , bg="transparent")
 par(mfrow=c(2,2))
 
-hist(as.numeric(dtSelectedDates$Global_active_power)/1000,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
-plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Global_active_power)/1000,xlab="",ylab="Global Active Powe (kilowatts)",pch=20)
-plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1),ylab="Energy sub metering",xlab="")
-plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Global_active_power)/1000,xlab="",ylab="Global Active Powe (kilowatts)",pch=20)
+#Plot1
+plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Global_active_power)/1000,xlab="",ylab="Global Active Power",pch="")
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Global_active_power)/1000)
 
+#Plot2
+plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Voltage)/10,xlab="datetime",ylab="Voltage",pch="",ylim=c(80,250))
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Voltage)/10)
+
+#Plot3
+
+plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1),ylab="Energy sub metering",xlab="",pch="",ylim=c(0,40))
+
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_1))
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_2),col="red",ylim=c(0,40))
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Sub_metering_3),col="blue",ylim=c(0,40))
+
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       pch=c("_","_","_"),col=c("black","red","blue"))
+
+
+#Plot4
+plot(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Global_reactive_power)/1000,xlab="datetime",ylab="Global_reactive_power",pch=20)
+lines(dtSelectedDates$KeyDateTime,as.numeric(dtSelectedDates$Global_reactive_power)/1000)
 
 dev.off()
 
